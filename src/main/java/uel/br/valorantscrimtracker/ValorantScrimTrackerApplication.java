b2f71a7e4e86ebd6d47c45a5d6cf404890219517
Fernando Henrique Russo <fh.russo2007@gmail.com>
2026-09-15 16:01:23 -0300

    Initial commit - Projeto Valorant Scrim Tracker

---
+++ src/main/java/uel/br/valorantscrimtracker/ValorantScrimTrackerApplication.java
@@ -0,0 +1,13 @@
+package uel.br.valorantscrimtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantScrimTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorantScrimTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uel/br/valorantscrimtracker/ValorantScrimTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package uel.br.valorantscrimtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantScrimTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
