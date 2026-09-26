0115ea57839eec3f14e8da91947f2bd81826bb87
Otávio Nazário Miranda <otavio.nazario@uel.br>
2026-09-26 13:38:04 -0300

    feat: adiciona página de listagem de partidas

--- src/main/java/uel/br/valorantscrimtracker/exception/GlobalExceptionHandler.java
+++ src/main/java/uel/br/valorantscrimtracker/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package uel.br.valorantscrimtracker.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
